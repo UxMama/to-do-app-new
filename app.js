6e5e2f142e0a5a7d8f3b4b561e35206e9e2e1e23
Tasha <latashasalgado@gmail.com>
2018-11-27 08:35:06 -0600

    AM6 completed

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
 function onReady() {
-  const toDos = [];
+  let toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
+  let id = 0;
 
-  //access new to do, put it in the array, and clear the field
   function createNewToDo() {
     const newToDoText = document.getElementById('newToDoText');
-    const addToDoForm = document.getElementById('addToDoForm');
-
-    //empty text check
     if (!newToDoText.value) { return; }
 
-    toDos.push({
+    toDos.push ({
       title: newToDoText.value,
-      complete: false
+      complete: false,
+      id: id
+
     });
+
+    id++;
 
     newToDoText.value = '';
 
     renderTheUI();
   }
 
-  //renders the UI
+  function deleteToDo(id) {
+    return toDos.filter(toDo => toDo.id !==id);
+  }
+
   function renderTheUI() {
     const toDoList = document.getElementById('toDoList');
-
     toDoList.textContent = '';
 
     toDos.forEach(function(toDo) {
@@ -31,25 +34,34 @@ function onReady() {
       const checkbox = document.createElement('input');
       checkbox.type = "checkbox";
 
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'x';
+
+      deleteButton.addEventListener('click', event => {
+        toDos = deleteToDo(toDo.id);
+        renderTheUI();
+      });
+
+
       newLi.textContent = toDo.title;
 
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
+      newLi.appendChild(deleteButton);
+
     });
   }
 
-  //event listener when the user submits addToDoForm
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
     createNewToDo();
   });
 
-  //call to render the UI
   renderTheUI();
-
 }
 
 
-window.onload = function() {
+window.onload = function () {
+  alert("The window has loaded!");
   onReady();
 };
